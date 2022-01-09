10a3577d83dbee9e0ae90667c42294962ca5860a
samahgado <samahgad85@gmail.com>
2022-01-09 09:04:49 +0200

    header-comp

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -3,5 +3,5 @@ const gulpSass = require("gulp-sass")
 const sass = gulpSass(require("sass"))
 
 gulp.task("sass",async function(){
-    gulp.src("src/*.scss").pipe(sass()).pipe(gulp.dest("src/css"))
+    gulp.src("src/component/**/*.scss").pipe(sass()).pipe(gulp.dest("src/css"))
 })
