9f3aab38903cfc87181752caf24c6d20b322fd7c
samahgado <samahgad85@gmail.com>
2022-01-11 08:31:47 +0200

    footer-comp

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -5,3 +5,9 @@ const sass = gulpSass(require("sass"))
 gulp.task("sass",async function(){
     gulp.src("src/component/**/*.scss").pipe(sass()).pipe(gulp.dest("src/css"))
 })
+//gulp.task("watch",async function(){
+    //gulp.watch("src/component/**/*.scss",async function(){
+      //  gulp.src("src/component/**/*.scss").pipe(sass()).pipe(gulp.dest("src/css"))
+
+    // })
+//})
